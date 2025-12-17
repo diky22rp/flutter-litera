b5458dc418dfa3d6d8442644179617ce6c5d3d82
Diky Rizki Pratama <diky.pratama9@gmail.com>
2025-12-17 10:55:25 +0700

    feat(core): implement base failure, usecase, and app entry point

---
+++ lib/injection_container.dart
@@ -0,0 +1,12 @@
+import 'package:get_it/get_it.dart';
+
+final sl = GetIt.instance;
+
+Future<void> init() async {
+  //External (Firebase)
+  //register FirebaseAuth & Firestore saat masuk fitur Auth
+
+  //Core
+
+  //Features - Auth
+}
